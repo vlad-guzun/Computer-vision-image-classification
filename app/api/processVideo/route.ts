86c8a6753effdc237934d61c2b17cf142a9e23ab
vlad-guzun <vladguzun20022@gmail.com>
2024-05-30 02:16:42 +0300

    deleted

--- app/api/processVideo/route.ts
+++ app/api/processVideo/route.ts
@@ -1,12 +1,11 @@
 // app/api/processVideo/route.ts
 import { NextRequest, NextResponse } from 'next/server';
-import { uploadAndProcessVideo } from "../../../lib/videoDownload";
 
 export async function GET(req: NextRequest) {
   const videoUrl = 'https://utfs.io/f/8111cb7b-788f-4f5f-ba7f-06ba7b5876e2-f11f4y.mp4'; // Hardcoded URL
 
   try {
-    const frames = await uploadAndProcessVideo(videoUrl);
+    const frames = "Vlad";
     console.log('Extracted frames:', frames);
     return NextResponse.json({ message: 'Video processed successfully', frames }, { status: 200 });
   } catch (error) {
