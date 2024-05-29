46550a96e24832e004c7c33f0f5e4ee7b5d89c36
vlad-guzun <vladguzun20022@gmail.com>
2024-05-30 01:56:14 +0300

    cloudinary

--- lib/videoDownload.ts
+++ lib/videoDownload.ts
@@ -21,7 +21,7 @@ export async function uploadAndProcessVideo(url: string): Promise<string[]> {
           crop: 'scale',
         },
       });
-      frameUrls.push(frameUrl);
+      frameUrls.push(frameUrl); //
     }
 
     return frameUrls;
